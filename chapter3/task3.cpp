c5b0be7ce1b3273e0abc9084c78ba11f0ea0ba09
ertadan <vin021110@gmail.com>
2023-05-07 00:25:24 +0300

    Removed hardcoded test array and unused variable

--- chapter3/task3.cpp
+++ chapter3/task3.cpp
@@ -14,16 +14,11 @@ using namespace std;
 int main () {
     srand(std::time(0)); //use current time as seed for random generator
     const int n = 20;
-    double  arr [n] = {
-        6.25,8.16667,7.33333,6.11111,7.125,7.14286,
-        4.11111,5,5.5,2.11111,9.14286,1.1,
-        9.125,3.1,3.11111,7.1,3.11111,5,0.333333,9
-    };
+    double  arr [n] ;
 
-    double sum = 0;
-    // for(int i = 0; i < n; i++){
-    //     arr[i]  = rand() % 10 + 1 / (double) ( rand() % 10 + 1);
-    // }
+    for(int i = 0; i < n; i++){
+        arr[i]  = rand() % 10 + 1 / (double) ( rand() % 10 + 1);
+    }
     for(int i = 0; i < n; i++) cout << arr[i] << " ";
     cout << endl;
 
